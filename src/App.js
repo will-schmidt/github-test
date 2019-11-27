11b8dc30165f73e008ec4d6f4901d9078892aaf3
Will Schmidt <will.schmidt@gmail.com>
2019-11-28 07:42:24 +1300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = props => <button onClick={props.onClick}>{props.label}</button>
+class Display extends React.Component {
+  render() {
+    return <p>{this.props.value}</p>
+  }
 }
 
-export default App;
+class App extends React.Component {
+  state = {
+    inputValue: '',
+    profile: null
+  }
+
+  handleClick = () => {
+    const url = `https://api.github.com/users/${this.state.inputValue}`
+    fetch(url)
+      .then(data => data.json())
+      .then(data => this.setState({ profile: data }))
+  }
+
+  handleChange = e => this.setState({ inputValue: e.target.value })
+
+  render() {
+    console.log('rendering', this.state.profile)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src={this.state.profile && this.state.profile.avatar_url}
+            className="App-logo"
+            alt="logo"
+          />
+          {this.state.profile && this.state.profile.name}
+          <Button onClick={this.handleClick} label="Click" />
+          <input onChange={this.handleChange} />
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
